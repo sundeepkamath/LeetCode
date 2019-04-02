1eff0516a7fd70e97feeec894ece116b3e319552
sundeepkamath <sundeep.g.kamath@gmail.com>
2019-04-02 22:15:33 +0530

    Palindrome and ReverseInteger

---
+++ LeetCode/Palindrome/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Palindrome
+{
+    class Palindrome
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] {1,4,4,1};
+
+            bool isPalindrome = IsPalindrome(nums);
+            Console.WriteLine($"isPalindrome: {isPalindrome}");
+        }
+
+        private static bool IsPalindrome(int[] nums)
+        {
+            int i = 0, j = nums.Length - 1;
+
+            while (i <= j)
+            {
+                if (nums[i++] != nums[j--])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/ReverseInteger/ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LeetCode.ReverseInteger
+{
+    public class ReverseInteger
+    {
+        public static void Main(string[] args)
+        {
+            int number = 12521;
+
+            Console.WriteLine(IsPalindrome(number));
+        }
+
+        private static bool IsPalindrome(int number)
+        {
+            if (number < 0 || number % 10 == 0)
+                return false;
+
+            int reverseNum = 0;
+            while (number > reverseNum)
+            {
+                reverseNum = reverseNum * 10 + (number % 10);
+                number /= 10;
+            }
+
+            return reverseNum == number || number == (reverseNum / 10);
+        }
+    }
+}
